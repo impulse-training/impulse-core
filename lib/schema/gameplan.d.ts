521238b32dd22c963e4ec8ca5ebff109af375131
Michael Dawson <m.dawson@y7mail.com>
2023-04-24 12:44:48 -0500

    Add featured flag to gameplans

--- lib/schema/gameplan.d.ts
+++ lib/schema/gameplan.d.ts
@@ -2,6 +2,7 @@ import { FakeTimestamp } from '../utils/FakeTimestamp';
 export interface GameplanValue {
     uid: string;
     name: string;
+    isFeatured: boolean;
     createdAt: FakeTimestamp;
     updatedAt: FakeTimestamp;
 }
--- src/schema/gameplan.ts
+++ src/schema/gameplan.ts
@@ -4,6 +4,7 @@ import { FakeTimestamp } from '../utils/FakeTimestamp';
 export interface GameplanValue {
   uid: string;
   name: string;
+  isFeatured: boolean;
   createdAt: FakeTimestamp;
   updatedAt: FakeTimestamp;
 }
