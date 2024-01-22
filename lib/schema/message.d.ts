9c0f43cb69db996d57b635d147d7beafdf24d814
Michael Dawson <m.dawson@y7mail.com>
2024-01-22 18:06:30 -0500

    Add deletedAt field to messages

--- lib/schema/message.d.ts
+++ lib/schema/message.d.ts
@@ -3,6 +3,7 @@ import { LogValue } from './logs';
 export interface MessageValue {
     createdAt: FakeTimestamp;
     updatedAt: FakeTimestamp;
+    deletedAt?: FakeTimestamp;
     fromUid: string;
     text: string;
     href?: string;
--- src/schema/message.ts
+++ src/schema/message.ts
@@ -4,6 +4,7 @@ import { LogValue } from './logs';
 export interface MessageValue {
   createdAt: FakeTimestamp;
   updatedAt: FakeTimestamp;
+  deletedAt?: FakeTimestamp;
   fromUid: string;
   text: string;
   href?: string;
