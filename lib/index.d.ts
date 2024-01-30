2d40deb97f9d6fcb2b18a698bfe9a185f4d21175
Michael Dawson <m.dawson@y7mail.com>
2024-01-30 10:41:13 -0500

    Add collections export

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -2,3 +2,4 @@ export * from './models';
 export * from './schema';
 export { sampleImages } from './sampleImages';
 export { TimestampLike } from './utils/TimestampLike';
+export declare const COLLECTIONS: string[];
--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,28 @@ export * from './schema';
 
 export { sampleImages } from './sampleImages';
 export { TimestampLike } from './utils/TimestampLike';
+
+export const COLLECTIONS = [
+  'applications',
+  'categories',
+  'challenges',
+  'contacts',
+  'credentials',
+  'emailSubscriptions',
+  'gameplans',
+  'issues',
+  'locations',
+  'logSummaries',
+  'logs',
+  'mail',
+  'mailTemplates',
+  'notifications',
+  'patterns',
+  'presses',
+  'profiles',
+  'recommendationRules',
+  'recommendations',
+  'reminders',
+  'supportGroups',
+  'tactics',
+];
