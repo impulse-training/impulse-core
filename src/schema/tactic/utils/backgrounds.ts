bfa69695bab18c848b5201267291805f6d78f906
Michael Dawson <m.dawson@y7mail.com>
2024-04-18 15:49:12 -0400

    Add tactic background images

---
+++ src/schema/tactic/utils/backgrounds.ts
@@ -0,0 +1,16 @@
+export const tacticBackgrounds = [
+  'tactics/dragon-scales.png',
+  'tactics/endless-constellation.png',
+  'tactics/flat-mountains.png',
+  'tactics/hollowed-boxes.png',
+  'tactics/liquid-cheese.png',
+  'tactics/quantum-gradient.png',
+  'tactics/radiant-gradient.png',
+  'tactics/rainbow-vortex.png',
+  'tactics/rose-petals.png',
+  'tactics/scattered-forcefields.png',
+  'tactics/slanted-gradient.png',
+  'tactics/subtle-prism.png',
+  'tactics/sun-tornado.png',
+  'tactics/wintery-sunburst.png',
+];
