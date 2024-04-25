1bbd13533a40a40af2397ae2e9b4aa574ae0d40c
Michael Dawson <m.dawson@y7mail.com>
2024-04-25 17:01:23 -0400

    Update profile logs summary

--- lib/schema/profileLogsSummary.d.ts
+++ lib/schema/profileLogsSummary.d.ts
@@ -1,31 +1,13 @@
 import * as yup from 'yup';
-export declare const profileLogsSummarySchema: yup.ObjectSchema<{
-    createdAt: {
-        seconds: number;
-        nanoseconds: number;
-        toDate: Function;
-    } | null | undefined;
-    updatedAt: {
-        seconds: number;
-        nanoseconds: number;
-        toDate: Function;
-    } | null | undefined;
-    profileId: string;
-    daySummaries: {
-        [x: string]: {
-            optionColors: string[];
-            tacticDaySummaries: {
-                [x: string]: {
-                    totalDataValue: number;
-                    formattedTotalDataValue: string;
-                };
+export declare const profileLogsSummarySchema: yup.Lazy<{
+    [x: string]: {
+        optionColors: string[];
+        tactics: {
+            [x: string]: {
+                totalDataValue: number;
+                formattedTotalDataValue: string;
             };
         };
     };
-}, yup.AnyObject, {
-    createdAt: undefined;
-    updatedAt: undefined;
-    profileId: undefined;
-    daySummaries: undefined;
-}, "">;
+}, yup.AnyObject, any>;
 export type ProfileLogsSummaryValue = yup.InferType<typeof profileLogsSummarySchema>;
--- src/schema/profileLogsSummary.ts
+++ src/schema/profileLogsSummary.ts
@@ -1,7 +1,6 @@
 import * as yup from 'yup';
 import { requiredStringArray } from './utils/array';
 import { objectOf } from './utils/objectOf';
-import { optionalTimestampSchema } from './utils/timestamp';
 
 const tacticDaySummarySchema = yup.object({
   totalDataValue: yup.number().required(),
@@ -10,15 +9,10 @@ const tacticDaySummarySchema = yup.object({
 
 const daySummarySchema = yup.object({
   optionColors: requiredStringArray,
-  tacticDaySummaries: objectOf(tacticDaySummarySchema),
+  tactics: objectOf(tacticDaySummarySchema),
 });
 
-export const profileLogsSummarySchema = yup.object({
-  createdAt: optionalTimestampSchema,
-  updatedAt: optionalTimestampSchema,
-  profileId: yup.string().required(),
-  daySummaries: objectOf(daySummarySchema),
-});
+export const profileLogsSummarySchema = objectOf(daySummarySchema);
 
 export type ProfileLogsSummaryValue = yup.InferType<
   typeof profileLogsSummarySchema
