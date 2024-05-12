fa40096e1ec90f829b4fa29bafd09a946aa8620a
Michael Dawson <m.dawson@y7mail.com>
2024-05-12 06:57:47 -0400

    Simplify getDeepLink method

--- lib/utils/urls.d.ts
+++ lib/utils/urls.d.ts
@@ -1,1 +1,1 @@
-export declare function getDeepLinkUrl(appEnv: 'staging' | 'production', target: string, params: Record<string, any>): string;
+export declare function getDeepLinkUrl(appEnv: 'staging' | 'production', target: string, params: Record<string, unknown>): string;
--- src/utils/urls.ts
+++ src/utils/urls.ts
@@ -1,27 +1,11 @@
-import { flatMap, isArray, isObject } from 'lodash';
-
 export function getDeepLinkUrl(
   appEnv: 'staging' | 'production',
   target: string,
-  params: Record<string, any> // Allow any type of values within the params object
+  params: Record<string, unknown>
 ): string {
   const scheme = appEnv === 'production' ? 'impulse://' : 'impulse-test://';
-
-  function serializeParams(
-    obj: Record<string, any>,
-    prefix: string = ''
-  ): string {
-    return flatMap(obj, (value, key) => {
-      const subKey = prefix ? `${prefix}[${key}]` : key;
-      if (isObject(value) && !isArray(value)) {
-        return serializeParams(value, subKey);
-      }
-      return `${encodeURIComponent(subKey)}=${encodeURIComponent(
-        String(value)
-      )}`;
-    }).join('&');
-  }
-
-  const queryString = serializeParams(params);
-  return `${scheme}${target}?${encodeURIComponent(queryString)}`;
+  const queryParams = new URLSearchParams(
+    params as Record<string, string>
+  ).toString();
+  return `${scheme}${target}?${queryParams}`;
 }
