01d27cecc3df2987e801aae0850eadcfd8fa4c22
Michael Dawson <m.dawson@y7mail.com>
2024-11-03 10:14:09 -0600

    Add timestamps to message schema

--- lib/schema/message.d.ts
+++ lib/schema/message.d.ts
@@ -1,11 +1,29 @@
 import * as yup from 'yup';
 export declare const messageSchema: yup.ObjectSchema<{
+    createdAt: {
+        isEqual?: any;
+        toMillis?: any;
+        toJSON?: any;
+        seconds: number;
+        nanoseconds: number;
+        toDate: Function;
+    } | null | undefined;
+    updatedAt: {
+        isEqual?: any;
+        toMillis?: any;
+        toJSON?: any;
+        seconds: number;
+        nanoseconds: number;
+        toDate: Function;
+    } | null | undefined;
     content: string | null;
     senderProfileId: string | undefined;
     files: (import("./utils/firestore").DocumentReferenceLike<unknown> | undefined)[] | undefined;
     externalId: string | undefined;
     role: NonNullable<"user" | "assistant" | "system" | "tool" | undefined>;
 }, yup.AnyObject, {
+    createdAt: undefined;
+    updatedAt: undefined;
     content: undefined;
     senderProfileId: undefined;
     files: "";
--- src/schema/message.ts
+++ src/schema/message.ts
@@ -1,7 +1,10 @@
 import * as yup from 'yup';
 import { documentReferenceSchema } from './utils/firestore';
+import { optionalTimestampSchema } from './utils/timestamp';
 
 export const messageSchema = yup.object({
+  createdAt: optionalTimestampSchema,
+  updatedAt: optionalTimestampSchema,
   content: yup.string().nullable().defined(),
   senderProfileId: yup.string(),
   files: yup.array().of(documentReferenceSchema),
